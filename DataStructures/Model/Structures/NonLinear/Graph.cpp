9a4a17d306268b18d7e5c1963d5ec40391066ea9
Yael_Amaya <yiap2610@gmail.com>
2018-03-27 13:54:07 -0600

    Made a tree class and deleted a useless/empty graph file

--- DataStructures/Model/Structures/NonLinear/Graph.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Graph.cpp
-//  DataStructures
-//
-//  Created by Amaya Penunuri, Yael on 3/19/18.
-//  Copyright © 2018 Amaya Penunuri, Yael. All rights reserved.
-//
-
-#include "Graph.hpp"
