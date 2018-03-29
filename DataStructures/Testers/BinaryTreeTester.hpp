afa1677323ab4d02d70c887a6ea64f59f99c4ee5
Yael_Amaya <yiap2610@gmail.com>
2018-03-29 13:47:10 -0600

    BinaryTreeSearch Stuff

--- DataStructures/Testers/BinaryTreeTester.hpp
+++ DataStructures/Testers/BinaryTreeTester.hpp
@@ -9,15 +9,16 @@
 #ifndef BinaryTreeTester_hpp
 #define BinaryTreeTester_hpp
 
+#include "../Model/Structures/BinarySearchTree.hpp"
 #include "../Model/Nodes/BinaryTreeNode.hpp"
 #include <iostream>
 
 class BinaryTreeTester
 {
 private:
-    
+    BinarySearchTree<int> testTree;
 public:
-    
+    void doTreeStuff();
 };
 
 #endif /* BinaryTreeTester_hpp */
